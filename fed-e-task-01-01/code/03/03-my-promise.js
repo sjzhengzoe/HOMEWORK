e9b39354ffaed14dbf903010ffc375776c777286
test <test@gufeifei-3.local>
2020-12-03 23:51:36 +0800

    update

--- fed-e-task-01-01/code/03/03-my-promise.js
+++ fed-e-task-01-01/code/03/03-my-promise.js
@@ -158,9 +158,47 @@ class MyPromise {
 }
 
 // 测试代码
-new MyPromise((resolve, reject) => {
-  resolve("success2");
-  // reject("failure2");
-})
-  .then((value) => console.log(value))
-  .catch((reason) => console.log("reason:", reason));
+// new MyPromise((resolve, reject) => {
+//   resolve("success2");
+//   // reject("failure2");
+// })
+//   .then((value) => console.log(value))
+//   .catch((reason) => console.log("reason:", reason));
+
+const fp = require("lodash/fp");
+let _underscore = fp.replace(/\W+/g, "_");
+const cars = [
+  { name: "Ferrari FF", horsepower: 660, dollar_value: 700000, in_stock: true },
+  {
+    name: "Spyker C12 Zagato",
+    horsepower: 650,
+    dollar_value: 648000,
+    in_stock: false,
+  },
+  {
+    name: "Jaguar XKR-S",
+    horsepower: 550,
+    dollar_value: 132000,
+    in_stock: false,
+  },
+  { name: "Audi R8", horsepower: 525, dollar_value: 114200, in_stock: false },
+  {
+    name: "Aston Martin One-77",
+    horsepower: 750,
+    dollar_value: 185000,
+    in_stock: true,
+  },
+  {
+    name: "Pagani Huayra",
+    horsepower: 700,
+    dollar_value: 130000,
+    in_stock: false,
+  },
+];
+const fn = fp.flowRight(
+  fp.map(_underscore),
+  fp.map((item) => fp.toLower(item)),
+  fp.map((item) => item.name)
+);
+
+console.log(fn(cars));
