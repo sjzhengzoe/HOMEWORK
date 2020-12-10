791d26ebeaba65d201b6a12f80ee49ef6a47224f
飞飞 <zhengsujie@zuzuche.com>
2020-12-10 15:19:05 +0800

    add

--- fed-e-task-01-02/code/code3.js
+++ fed-e-task-01-02/code/code3.js
@@ -3,21 +3,48 @@
 // };
 // fn('zoe');
 
-function Person() {
-  this.name = "name";
-  this.age = 20;
-  this.fn = () => {
-    return this.age;
-  };
+// function Person() {
+//   this.name = "name";
+//   this.age = 20;
+//   this.fn = () => {
+//     return this.age;
+//   };
+// }
+
+// const p1 = new Person();
+// const age = p1.fn();
+
+// function Person() {
+//   this.name = "name";
+//   this.age = 20;
+// }
+
+// const p1 = new Person();
+// const age = p1.age;
+
+function fn(obj) {
+  if (obj.a) {
+    obj.a();
+  } else if (obj.b) {
+    obj.b();
+  } else {
+    obj.c();
+  }
+}
+// console.log(fn({ b: () => console.log(1) }));
+// 惰性函数
+function fn(obj) {
+  if (obj.a) {
+    fn = obj.a;
+  } else if (obj.b) {
+    fn = obj.b;
+  } else {
+    fn = obj.c;
+  }
+  return fn();
 }
 
-const p1 = new Person();
-const age = p1.fn();
-
-function Person() {
-  this.name = "name";
-  this.age = 20;
-}
-
-const p1 = new Person();
-const age = p1.age;
+console.log(fn({ b: () => console.log(1) }));
+console.log(fn({ b: () => console.log(1) }));
+console.log(fn({ b: () => console.log(1) }));
+console.log(fn({ b: () => console.log(1) }));
