4e50163453bfa01d3d41fd0f1a9af4dded2a8e2e
飞飞 <zhengsujie@zuzuche.com>
2021-01-14 14:44:42 +0800

    初始化

---
+++ fed-e-task-03-03/code/NuxtTest/pages/index.vue
@@ -0,0 +1,21 @@
+<!-- 首页 -->
+<template>
+  <div class="home">首页</div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {},
+};
+</script>
+<style scoped></style>
