cbaeb6b96212fb228afc8f08db763998ba89fc7d
飞飞 <zhengsujie@zuzuche.com>
2021-01-22 18:00:40 +0800

    add

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,23 +1,17 @@
 module.exports = function(api) {
   // 获取数据
-  api.loadSource(({ addCollection }) => {
-    // const { posts } = await axios.get('https://api.example.com/posts')
-    // const collection = actions.addCollection({
-    //   typeName: 'BlogPosts'
-    // })
-    // for (const post of posts) {
-    //   collection.addNode({
-    //     id: post.id,
-    //     title: post.title
-    //   })
-    // }
-  });
+  api.loadSource(({ addCollection }) => {});
 
   // 创建页面
   api.createPages(({ createPage }) => {
     createPage({
       path: "/projects/:type",
       component: "./src/templates/Projects.vue",
+    });
+
+    createPage({
+      path: "/journal/:type",
+      component: "./src/templates/Journal.vue",
     });
   });
 };
