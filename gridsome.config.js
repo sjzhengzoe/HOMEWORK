cdd2e6644dc0ee3d01bbf852308dcffea0638e30
飞飞 <zhengsujie@zuzuche.com>
2021-01-26 11:39:16 +0800

    暂时完成

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,8 +4,15 @@ module.exports = {
     {
       use: "@gridsome/source-filesystem",
       options: {
-        path: "assets/data/**/*.json",
-        typeName: "BlogDatas",
+        path: "./src/assets/data/journal/**/*.json",
+        typeName: "journalData",
+      },
+    },
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "./src/assets/data/projects/**/*.json",
+        typeName: "projectsData",
       },
     },
   ],
