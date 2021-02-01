5d3d4721846e2d7731576b47731701e3c29dc5a0
飞飞 <zhengsujie@zuzuche.com>
2021-02-01 17:40:08 +0800

    整理第一模块

--- fed-e-task-01-01/code/01/1/16-compose-associativit.js
+++
@@ -1,6 +0,0 @@
-const _ = require("lodash");
-// const f = _.flowRight(_.toUpper, _.first, _.reverse);
-// const f = _.flowRight(_.flowRight(_.toUpper, _.first), _.reverse);
-const f = _.flowRight(_.toUpper, _.flowRight(_.first, _.reverse));
-
-console.log(f(["one", "two", "three"]));
