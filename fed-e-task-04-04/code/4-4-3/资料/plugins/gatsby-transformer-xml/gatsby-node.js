2e693fa89ea295fd425c898481ada768ee0eaeaf
飞飞 <zhengsujie@zuzuche.com>
2021-02-22 11:24:01 +0800

    add

--- fed-e-task-04-04/code/4-4-3/资料/plugins/gatsby-transformer-xml/gatsby-node.js
+++ fed-e-task-04-04/code/4-4-3/资料/plugins/gatsby-transformer-xml/gatsby-node.js
@@ -1,24 +1,27 @@
-const { parseString } = require("xml2js")
-const { promisify } = require("util")
-const parse = promisify(parseString)
-const createNodeHelpers = require("gatsby-node-helpers").default
+const { parseString } = require("xml2js");
+const { promisify } = require("util");
+const parse = promisify(parseString);
+const createNodeHelpers = require("gatsby-node-helpers").default;
 
 async function onCreateNode({ node, loadNodeContent, actions }) {
-  const { createNode } = actions
+  const { createNode } = actions;
   if (node.internal.mediaType === "application/xml") {
-    const xml = await loadNodeContent(node)
+    // 获取xml内容
+    const xml = await loadNodeContent(node);
+    // xml数据转换成对象
     const obj = await parse(xml, {
       explicitArray: false,
       explicitRoot: false,
-    })
+    });
     const { createNodeFactory } = createNodeHelpers({
       typePrefix: "XML",
-    })
-    const createNodeObject = createNodeFactory("parsedContent")
-    createNode(createNodeObject(obj))
+    });
+    // 将xml数据添加到数据层
+    const createNodeObject = createNodeFactory("parsedContent");
+    createNode(createNodeObject(obj));
   }
 }
 
 module.exports = {
   onCreateNode,
-}
+};
